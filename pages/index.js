ed93b7f87cca5e8be26f6411eb266d33f12b43ea
HyangRim <curomame@naver.com>
2022-01-05 23:26:46 +0900

    next js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
 import HeadInfo from "../components/HeadInfo"
 
-export default function Home() {
+export default function Home({posts}) {
+  
+  console.log(posts);
+  
   return (
     <div>
       <HeadInfo/>
       <h1>Welcome to My Blog</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+//서버 사이드 랜더링 => 페이지에 들어올때마다 서버에 요청을 해서 데이터를 받아옴
+//즉각적으로 데이터의 변화가 일어날때 사용
+
+// export const getServerSideProps = async() => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_end=10')
+//   const posts = await res.json();
+
+//   return {
+//     props : {
+//       posts
+//     }
+    
+//   }
+// }
+
+// next 에서는 스태틱을 추천함
+// node js에서 해도 안바낌
+// 인크리먼트 스태틱 리 제네레이션
+
+export const getStaticProps = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_end=10')
+  const posts = await res.json();
+
+  return {
+    props : {
+      posts
+    },
+    revalidate : 20
+    // 처음 접속이 일어난 후 20초 후에 새로 리 제네레이션 시킬 수 있도록 명령을 진행하는 옵션
+    // 즉 즉각적인 변동이 필요한게 아니라면, 이런식으로 진행을 해주면 된다는 것이다.
+  }
 }
--- pages/photos.js
+++ pages/photos.js
@@ -1,12 +1,41 @@
 import HeadInfo from "../components/HeadInfo"
+import Image from 'next/image'
+import photosStyles from "../styles/Photos.module.css"
+import Link from 'next/link'
 
-const photos = () => {
+const photos = ({photos}) => {
   return (
     <div>
       <HeadInfo title="My Blog Photos"/>
       <h1>My Photos</h1>
+      <ul className={photosStyles.photos}>
+        {photos.map(photo => (
+          <li key = {photo.id}>
+            <Link href={`/photos/${photo.id}`}>
+            <Image
+            src ={photo.thumbnailUrl}
+            width={100}
+            height={100}
+            alt = {photo.title}
+            />
+            </Link>
+            <span>{photo.title}</span>
+            </li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 export default photos
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_end=10')
+  const photos = await res.json();
+
+  return {
+    props : {
+      photos
+    }
+  }
+}
